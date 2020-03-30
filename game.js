39ff190cb407827fa700ec494e615c3947df972c
Kenan <xinkenan.11@hotmail.com>
2020-03-30 15:15:14 +0800

    :bug: bind this to calling object

--- game.js
+++ game.js
@@ -6,32 +6,32 @@ let popup = null;
 let gamePlatform = null;
 
 class GamePlatForm {
-  constructor() {
+  constructor(gamePiece) {
     this.canvas = document.createElement("canvas");
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
-    this.keys = [];
     this.frameNo = 0;
-
+    this.gamePiece = gamePiece;
   }
   start() {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     // todo: make setinterval this reference calling object instead of window
-    this.interval = setInterval(this.update, 25);
+    this.interval = setInterval(this.update.bind(this), 25);
     this.setListeners();
     this.x = 0;
     this.y = 0;
   }
 
   setListeners() {
+    const _this = this;
     window.addEventListener('keydown', function (e) {
-      this.keys = (this.keys || []);
-      this.keys[e.keyCode] = true;
+      window.keys = (window.keys || []);
+      window.keys[e.keyCode] = true;
     });
     window.addEventListener('keyup', function (e) {
-      this.keys[e.keyCode] = false;
-      gamePiece.stopMove();
+      window.keys[e.keyCode] = false;
+      _this.gamePiece.stopMove();
     });
   }
 
@@ -46,39 +46,39 @@ class GamePlatForm {
   update() {
     for (let i = 0; i < enemies.length; i++) {
 
-      if (gamePiece.hasCollidedWith(enemies[i])) {
-        gamePlatform.stop();
-        //alert("<= Game Reborn =>")
+      if (this.gamePiece.hasCollidedWith(enemies[i])) {
+        this.stop();
+        alert("<= It is not a game over. It is a fresh beginning. =>");
         return;
       }
 
     }
 
     for (let i = 0; i < food.length; i++) {
-      if (gamePiece.hasCollidedWith(food[i])) {
+      if (this.gamePiece.hasCollidedWith(food[i])) {
         popup.x = food[i].x + 15;
         popup.y = food[i].y + 10;
         food.splice(i, 1);
         popup.text = "+ Bonus:" + "100";
 
-        gamePlatform.frameNo += 100;
+        this.frameNo += 100;
         setTimeout(function () {
           popup.text = "";
         }, 2000)
       }
     }
 
-    if (gamePlatform.x && gamePlatform.y) {
-      gamePiece.x = gamePlatform.x;
-      gamePiece.y = gamePlatform.y;
+    if (this.x && this.y) {
+      this.gamePiece.x = this.x;
+      this.gamePiece.y = this.y;
     }
 
-    gamePlatform.clear();
-    gamePlatform.frameNo++;
+    this.clear();
+    this.frameNo++;
 
-    if (gamePlatform.frameNo == 1 || isInterval(50)) {
-      let newComponentX = gamePlatform.canvas.width;
-      let maxHeight = gamePlatform.canvas.height - 10;
+    if (this.frameNo == 1 || this.isInterval(50)) {
+      let newComponentX = this.canvas.width;
+      let maxHeight = this.canvas.height - 10;
       let minHeight = 10;
 
       enemies.push(new GameComponent(10, 10, "#679b9b", newComponentX, Math.floor((Math.random() * maxHeight) + minHeight)));
@@ -95,28 +95,34 @@ class GamePlatForm {
       food[i].render()
     }
 
-    if (this.keys && this.keys[37]) {
-      gamePiece.moveLeft();
+    if (window.keys && window.keys[37]) {
+      this.gamePiece.moveLeft();
     }
-    if (this.keys && this.keys[39]) {
-      gamePiece.moveRight();
+    if (window.keys && window.keys[39]) {
+      this.gamePiece.moveRight();
     }
-    if (this.keys && this.keys[38]) {
-      gamePiece.moveUp();
+    if (window.keys && window.keys[38]) {
+      this.gamePiece.moveUp();
     }
-    if (this.keys && this.keys[40]) {
-      gamePiece.moveDown();
+    if (window.keys && window.keys[40]) {
+      this.gamePiece.moveDown();
     }
 
-    score.text = "SCORE: " + gamePlatform.frameNo;
+    score.text = "SCORE: " + this.frameNo;
     score.render();
     if (popup) {
       popup.render();
     }
-    gamePiece.updatePos();
-    gamePiece.render();
+    this.gamePiece.updatePos();
+    this.gamePiece.render();
+  }
+
+  isInterval(n) {
+    if ((this.frameNo / n) % 1 == 0) return true;
+    return false;
   }
 }
+
 
 class GameComponent {
   constructor(width, height, color, x, y, type) {
@@ -191,17 +197,13 @@ class GameComponent {
     this.velocityX = 0;
     this.velocityY = 0;
   }
-}
 
-function isInterval(n) {
-  if ((gamePlatform.frameNo / n) % 1 == 0) return true;
-  return false;
 }
 
 
 function startGame() {
-  gamePlatform = new GamePlatForm();
   gamePiece = new GameComponent(30, 30, "#f76a8c", 10, 120);
+  gamePlatform = new GamePlatForm(gamePiece);
   score = new GameComponent("14px", "Open Sans", "#679b9b", 10, 20, "text");
   popup = new GameComponent("12px", "Lato", "#fcf8f3", 100, 80, "popup");
   popup.text = "";
